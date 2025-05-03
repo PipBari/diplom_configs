799c1fa40db9831d71c38fbf4147cfe8652d29d8
Dmitriy Fetisov <PipBari@gmail.com>
2025-05-03 16:08:16 +0300

    Добавлен автоматический deploy.sh

---
+++ my-app/deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+
+echo "Начинается деплой dasdas..."
+cd /home/ubuntu/apps/dasdas
+
+echo "GIT_USERNAME: $GIT_USERNAME"
+echo "GIT_TOKEN длина: ${#GIT_TOKEN} символов"
+
+if [ ! -d .git ]; then
+  echo 'Репозиторий не найден, клонируем...'
+  git clone https://${GIT_USERNAME}:${GIT_TOKEN}@github.com/PipBari/diplom_configs repo_tmp
+  cp -r repo_tmp/* .
+  cp -r repo_tmp/.* . || true
+  rm -rf repo_tmp
+else
+  echo 'Репозиторий уже существует, пропускаем клонирование'
+fi
+
+echo 'Текущий коммит:'
+git log -1 --oneline || true
+
+if command -v ansible-playbook >/dev/null 2>&1; then
+  echo 'Ansible detected'
+  find . -type f \( -name "*.yml" -o -name "*.yaml" \) | while read f; do
+    if [[ "$f" == *".github/workflows/"* ]]; then
+      echo "Пропуск служебного файла: $f"
+      continue
+    fi
+    echo "→ Запуск Ansible playbook: $f"
+    ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook "$f" -i localhost, -c local --ask-become-pass || echo "⚠ Ошибка при запуске $f"
+  done
+else
+  echo 'Ansible не установлен на сервере'
+  exit 1
+fi
+
+echo "Деплой завершён"
