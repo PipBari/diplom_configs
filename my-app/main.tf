db0663ff4f6c6cac93fb6e70586c3e865f3e3d20
Dmitriy Fetisov <PipBari@gmail.com>
2025-05-03 19:44:08 +0300

    Обновление файла my-app/main.tf

--- my-app/main.tf
+++ my-app/main.tf
@@ -1,25 +1,12 @@
-terraform {
-  required_providers {
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.0"
-    }
-  }
-}
-
 provider "local" {}
 
-resource "local_file" "file1" {
-  filename = "${path.module}/file1.txt"
-  content  = "Это файл 1"
-}
+resource "null_resource" "test_vm" {
+  triggers = {
+    cpu = var.cpu
+    ram = var.ram
+  }
 
-resource "local_file" "file2" {
-  filename = "${path.module}/file2.txt"
-  content  = "Это файл 2"
-}
-
-resource "local_file" "file3" {
-  filename = "${path.module}/file3.txt"
-  content  = "Это файл 3"
+  provisioner "local-exec" {
+    command = "echo Creating VM with ${var.cpu} vCPU and ${var.ram} MB RAM"
+  }
 }
