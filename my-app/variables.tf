9a78eedb9dcb4f9a21dfdd5bd56f73f957408a51
Dmitriy Fetisov <PipBari@gmail.com>
2025-05-03 19:44:21 +0300

    Обновление файла my-app/variables.tf

---
+++ my-app/variables.tf
@@ -0,0 +1,11 @@
+variable "cpu" {
+  description = "Количество виртуальных CPU"
+  type        = number
+  default     = 2
+}
+
+variable "ram" {
+  description = "Объём оперативной памяти в МБ"
+  type        = number
+  default     = 2048
+}
