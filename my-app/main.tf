fba25354983bc54a504e13b8a4e6af72b13e2a4d
Dmitriy Fetisov <PipBari@gmail.com>
2025-05-03 19:19:24 +0300

    Обновление файла my-app/main.tf

--- my-app/main.tf
+++ my-app/main.tf
@@ -1,21 +1,49 @@
-# main.tf
 terraform {
   required_providers {
-    local = {
-      source  = "hashicorp/local"
-      version = "2.4.0"
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+      version = "0.6.14"
     }
   }
 }
 
-provider "local" {}
-
-resource "local_file" "hello_file_1" {
-  filename = "file1.txt"
-  content  = "Привет из первого ресурса"
+provider "libvirt" {
+  uri = "qemu:///system"
 }
 
-resource "local_file" "hello_file_2" {
-  filename = "file2.txt"
-  content  = "Привет из второго ресурса"
+resource "libvirt_volume" "ubuntu_volume" {
+  name   = "ubuntu-min"
+  pool   = "default"
+  source = "https://cloud-images.ubuntu.com/minimal/releases/focal/release/ubuntu-20.04-minimal-cloudimg-amd64.img"
+  format = "qcow2"
+}
+
+resource "libvirt_domain" "ubuntu_vm" {
+  name   = "ubuntu-vm"
+  memory = 512
+  vcpu   = 1
+
+  disk {
+    volume_id = libvirt_volume.ubuntu_volume.id
+  }
+
+  network_interface {
+    network_name = "default"
+  }
+
+  console {
+    type        = "pty"
+    target_port = "0"
+    target_type = "serial"
+  }
+
+  graphics {
+    type        = "vnc"
+    listen_type = "address"
+    autoport    = true
+  }
+
+  boot_device {
+    dev = ["hd"]
+  }
 }
