f3bf8b68e16c6080b7bdb2f9e556f938a91c57aa
Dmitriy Fetisov <PipBari@gmail.com>
2025-05-06 12:54:10 +0300

    Обновление файла my-app/main.tf

--- my-app/main.tf
+++ my-app/main.tf
@@ -5,6 +5,6 @@ variable "ram_mb" {
 }
 
 resource "local_file" "ram_info" {
-  content  = "Выделено памяти: ${var.ram_mb} MB"
+  content  = "Выделено памяти: ${var.ram_mb} MB\n"
   filename = "ram_info.txt"
 }
