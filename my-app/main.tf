fcbfd3a7500785403ce561835c4631db411c0e9e
Dmitriy Fetisov <PipBari@gmail.com>
2025-05-06 12:49:45 +0300

    Обновление файла my-app/main.tf

--- my-app/main.tf
+++ my-app/main.tf
@@ -1,36 +1,10 @@
-provider "libvirt" {
-  uri = "qemu:///system"
+variable "ram_mb" {
+  description = "Объем оперативной памяти в мегабайтах"
+  type        = number
+  default     = 512
 }
 
-resource "libvirt_volume" "ubuntu_qcow2" {
-  name   = "ubuntu-volume"
-  pool   = "default"
-  source = "https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img"
-  format = "qcow2"
-}
-
-resource "libvirt_domain" "ubuntu_vm" {
-  name   = "test-vm"
-  memory = var.ram_mb
-  vcpu   = 1
-
-  disk {
-    volume_id = libvirt_volume.ubuntu_qcow2.id
-  }
-
-  network_interface {
-    network_name = "default"
-  }
-
-  console {
-    type        = "pty"
-    target_port = "0"
-    target_type = "serial"
-  }
-
-  graphics {
-    type = "vnc"
-    listen_type = "address"
-    autoport = true
-  }
+resource "local_file" "ram_info" {
+  content  = "Выделено памяти: ${var.ram_mb} MB"
+  filename = "ram_info.txt"
 }
