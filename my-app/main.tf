039663dccf16cb2b37a4821a97c34f40c94557da
Dmitriy Fetisov <PipBari@gmail.com>
2025-05-12 10:37:28 +0300

    Обновление файла my-app/main.tf

--- my-app/main.tf
+++ my-app/main.tf
@@ -1,7 +1,7 @@
 variable "ram_mb" {
   description = "Объем оперативной памяти в мегабайтах"
   type        = number
-  default     = 512
+  default     = 256
 }
 
 resource "local_file" "ram_info" {
