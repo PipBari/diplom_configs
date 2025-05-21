0a5357e7078924c40a51e6319a8619dfef8233eb
Dmitriy Fetisov <PipBari@gmail.com>
2025-05-21 17:24:04 +0300

    Обновление файла my-app/123.sh

--- my-app/123.sh
+++ my-app/123.sh
@@ -1,1 +1,20 @@
-123213123
+#!/bin/bash
+
+echo "Подготовка окружения..."
+
+TARGET_DIR="./output"
+
+if [ ! -d "$TARGET_DIR" ]; then
+  mkdir "$TARGET_DIR"
+  echo "Папка $TARGET_DIR создана"
+else
+  echo "Папка $TARGET_DIR уже существует"
+fi
+
+echo "Скрипт выполнен $(date)" > "$TARGET_DIR/result.txt"
+echo "Файл result.txt создан в $TARGET_DIR"
+
+echo "Содержимое файла:"
+cat "$TARGET_DIR/result.txt"
+
+echo "Готово"
